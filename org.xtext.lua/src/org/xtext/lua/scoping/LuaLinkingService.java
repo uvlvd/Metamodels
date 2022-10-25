133d79367ac92ad27a8ff67e7458c81dfd464fea
Lukas Burgey <lukas.burgey@student.kit.edu>
2022-10-25 12:45:40 +0200

    Add linking service

--- org.xtext.lua/src/org/xtext/lua/scoping/LuaLinkingService.java
+++ org.xtext.lua/src/org/xtext/lua/scoping/LuaLinkingService.java
@@ -19,15 +19,26 @@ import org.xtext.lua.lua.Referenceable;
 public class LuaLinkingService extends DefaultLinkingService {
     private static final Logger LOGGER = Logger.getLogger("LuaLinkingService");
     private static final URI mockUri = URI.createURI("dummy:/stdlibAndCrowns.lua");
-
-    private EObject getOrCreateMockReferenceable(EObject context, String name) {
+    
+    /**
+     * Creates a dummy resource in the contexts resource set
+     */
+    private Resource getOrCreateMockResource(EObject context) {
         // create a dummy URI with the DSL's file extension
         ResourceSet resourceSet = context.eResource()
             .getResourceSet();
-        Resource resource = resourceSet.getResource(mockUri, true);
 
+        Resource resource = resourceSet.getResource(mockUri, false);
+        if (resource == null ) {
+            resource = resourceSet.createResource(mockUri);
+        }
+        return resource;
+    }
+
+    private EObject getOrCreateMockReferenceable(EObject context, String name) {
+        var mockResource = getOrCreateMockResource(context);
         // if we already created a mock refble we return it
-        for (Iterator<EObject> i = resource.getAllContents(); i.hasNext();) {
+        for (Iterator<EObject> i = mockResource.getAllContents(); i.hasNext();) {
             var eObj = i.next();
             var refble = (Referenceable) eObj;
             if (refble.getName()
@@ -41,7 +52,7 @@ public class LuaLinkingService extends DefaultLinkingService {
         var refble = LuaFactory.eINSTANCE.createReferenceable();
         refble.setName(name);
 
-        List<EObject> contents = resource.getContents();
+        List<EObject> contents = mockResource.getContents();
         contents.add(refble);
         return refble;
     }

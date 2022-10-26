5bcfa24d7edb2e1f3e4bf799fd6a30ad512edb7e
Lukas Burgey <lukas.burgey@student.kit.edu>
2022-10-26 15:26:40 +0200

    Replace SuperChunk with ChunkSet

--- org.xtext.lua/src/org/xtext/lua/lua/SuperChunk.java
+++
@@ -1,8 +0,0 @@
-package org.xtext.lua.lua;
-
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.ecore.EObject;
-
-public interface SuperChunk extends EObject {
-  EList<Chunk> getChunks();
-}
